363fdc0c7f544219068c8b9368ed645254a490ee
Andrew Lukichev <andrewlukichev@gmail.com>
2015-12-02 16:51:18 +0300

    [DEV] social sharing

--- src/article/article.controller.js
+++ src/article/article.controller.js
@@ -2,11 +2,12 @@ angular
     .module('buzz-web.article')
     .controller('ArticleController', ArticleController);
 
-ArticleController.$inject = ['article', '$sce'];
+ArticleController.$inject = ['article', '$sce', '$location'];
 
-function ArticleController(article, $sce) {
+function ArticleController(article, $sce, $location) {
     var self = this;
     self.article = article;
+    self.location = $location;
 
     self.url = $sce.trustAsResourceUrl(article.url.replace(new RegExp('%2F','g'),'/'));
 }
