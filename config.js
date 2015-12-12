5743c64c00a4bb0924d943ced3584146724c700d
James Rhodes <jrhodes@weddingwire.com>
2015-12-11 19:00:57 -0500

    add feed_id config variable

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
     api_url : process.env.API_URL,
-
+    feed_id : process.env.FEED_ID,
     twitter_api_key : process.env.TWITTER_API_KEY,
     twitter_secret : process.env.TWITTER_SECRET,
 
